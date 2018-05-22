d558272545c6f6fa0d8d82fb21a3b6046417337d
MY <MY@MY-PC>
2018-05-22 16:26:16 +0800

    20180522

---
+++ src/com/poac/quickview/gui/MainApp.java
@@ -0,0 +1,43 @@
+package com.poac.quickview.gui;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		initRootLayout();
+	}
+	
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("MainForm.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
